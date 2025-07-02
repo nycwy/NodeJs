6909e912f420ac85fb3225c1ba16758b45e0dbce
nycwy <stuxy.1129@gmail.com>
2025-07-03 00:42:04 +0530

    HTTP Methods (GET and POST)

--- Server/index.js
+++ Server/index.js
@@ -4,18 +4,24 @@ const url = require('url');
 
 const myServer = http.createServer((req, res) => {
     if (req.url === "/favicon.ico") return res.end();
-    const log = `${Date.now()}: ${req.url} New request received\n`;
+    const log = `${Date.now()}: ${req.method} ${req.url} New request received\n`;
     const myUrl = url.parse(req.url, true);
     console.log(myUrl);
     fs.appendFile('log.txt', log, (err, data) => {
         switch (myUrl.pathname) {
             case "/":
-                res.end("HomePage");
+                if(req.method === 'GET') res.end("HomePage");
                 break;
             case "/about":
                 const username = myUrl.query.myname;
                 res.end(`Hi ${username}`);
                 break;
+            case "/signup":
+                if (req.method === 'GET') res.end("This is a signup form");
+                else if(req.method === 'POST'){
+                    // DB Query
+                    res.end("Success");
+                }
             default:
                 res.end("404 Not Found");
         }
